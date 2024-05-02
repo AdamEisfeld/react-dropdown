b8e33a00fbc0ef146d5fea25fe1ff1d592983c40
Adam Eisfeld <adam.eisfeld@gmail.com>
2024-05-02 01:23:11 -0400

    1337: Hook Cleanup
    - Cleaning up custom hooks, moving logic out of TemplateTriggerReveal
    - Allowing styling to be passed in to dropdown component

--- src/components/molecules/DropdownMenuWrapperButton.tsx
+++ src/components/molecules/DropdownMenuWrapperButton.tsx
@@ -14,7 +14,7 @@ function DropdownMenuWrapperButton(props: DropdownMenuWrapperButtonProps) {
 
 	return (
 		<button onClick={onClick} onFocus={onFocus} className={`
-			p-4 transition-all duration-300 hover:bg-slate-700 bg-slate-800 text-white border-t border-slate-700 outline-none focus:bg-slate-500 active:bg-slate-500
+			outline-none focus:outline-none
 			${className}
 		`}>
 			{ props.children }
